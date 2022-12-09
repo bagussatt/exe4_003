570ae42fb0a891dbf60948305f879c1f61e82cc0
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2022-12-09 15:39:38 +0700

    membuat class stack  dan public stack

--- exe4_003/Program.cs
+++ exe4_003/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+namespace exe4_003
+{
+    class stack
+    {
+        private String[] array = new string[23];
+        private int top;
+        private int max;
+
+        public Stack()
+        {
+            top = -1;
+            max = array.Length;
+        }
+
+    }
+}
